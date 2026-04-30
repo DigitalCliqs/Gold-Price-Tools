4c5fc55efd3d6aa73bfd31c97fe5af8dd7ab5eaf
DigitalCliqs <181574200+DigitalCliqs@users.noreply.github.com>
2026-04-30 17:17:28 +0100

    chore: Remove Jules artifact commit_helper3.py

--- commit_helper3.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_cmd(cmd):
-    return subprocess.run(cmd, shell=True, capture_output=True, text=True)
-
-res = run_cmd("git push origin feature/issue-133-gold-silver-ratio")
-print(res.stdout)
-print(res.stderr)
